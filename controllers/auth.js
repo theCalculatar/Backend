36bbde4157b0dc15d047c12a83d35ab7adfd6376
theCalculatar <alpheusm13@gmail.com>
2024-11-29 21:19:27 +0200

    fixed dynamic status code on auth file, and added login logic to sign users

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const pool = require("../config/database");
-const { addUser } = require("../models/user");
+const { addUser, verifyDetails } = require("../models/user");
 
 const register = async (req, res) => {
   const { name, surname, email, password, role } = req.body;
@@ -9,8 +9,7 @@ const register = async (req, res) => {
     await addUser(name, surname, email, password, role);
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
+    res.status(error.code).json(error);
   }
 };
 
@@ -18,29 +17,19 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await pool.query('SELECT * FROM "user" WHERE email = $1', [
-      email,
-    ]);
+    const user = await verifyDetails(email, password);
 
-    if (user.rows.length === 0) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    const { userID, password: hashedPassword, role } = user.rows[0];
-
-    const isMatch = await bcrypt.compare(password, hashedPassword);
-    if (!isMatch) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    const token = jwt.sign({ userID, role }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign(
+      { userID: user.userid, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
 
     res.json({ message: "Login successful", token });
   } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(error.code).json(error);
   }
 };
 
--- models/user.js
+++ models/user.js
@@ -4,19 +4,13 @@ const { default: CustomError } = require("../errors");
 
 //Get users
 const getUser = async (email) => {
-  console.log("error");
   try {
     const user = await pool.query('SELECT * FROM "user" WHERE email = $1', [
       email,
     ]);
     return user.rows[0] || null;
   } catch (error) {
-    console.log(error);
-
-    throw new CustomError({
-      error: "Server error",
-      code: 500,
-    });
+    throw new CustomError("Server error", 500);
   }
 };
 
@@ -27,21 +21,33 @@ const addUser = async (name, surname, email, password, role) => {
     throw new CustomError("User already exists", 400);
   }
 
-  console.log("registeredUser.rows[0]");
-
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
     const registeredUser = await pool.query(
-      'INSERT INTO "user" (name, surname, email, password, role) VALUES ($1, $2, $3, $4, $5)',
+      'INSERT INTO "user" (name, surname, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [name, surname, email, hashedPassword, role || "respondent"]
     );
-    console.log(registeredUser.rows[0]);
     return registeredUser.rows[0];
   } catch (error) {
     console.log("error");
-    throw new CustomError(("Server error", 500));
+    throw new CustomError("Server error", 500);
   }
 };
 
-module.exports = { addUser };
+const verifyDetails = async (email, password) => {
+  const user = await getUser(email); // throws if any db errors
+
+  if (user == null) {
+    throw new CustomError("Invalid user details", 401);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  if (bcrypt.compare(hashedPassword, user.password)) {
+    return user;
+  } else {
+    throw new CustomError("Invalid user details", 401);
+  }
+};
+
+module.exports = { addUser, verifyDetails };
