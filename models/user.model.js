6520d54ba6e935637747bf89c066e82423319f03
theCalculatar <alpheusm13@gmail.com>
2024-12-15 11:26:34 +0200

    change pool to Prisma Orm:

--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,21 @@
-const pool = require("../config/database");
+// const pool = require("../config/database");
 const bcrypt = require("bcrypt");
 const { default: CustomError } = require("../errors");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 //Get users
 const getUser = async (email) => {
   try {
-    const user = await pool.query('SELECT * FROM "user" WHERE email = $1', [
-      email,
-    ]);
-    return user.rows[0] || null;
+    // const user = await pool.query('SELECT * FROM "user" WHERE email = $1', [
+    //   email,
+    // ]);
+    const user = await prisma.user.findFirst({
+      where: { email: email },
+    });
+    // return user.rows[0] || null;
+    return user;
   } catch (error) {
     throw new CustomError("Server error", 500);
   }
@@ -25,11 +32,21 @@ const addUser = async (name, surname, email, password, role) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    const registeredUser = await pool.query(
-      'INSERT INTO "user" (name, surname, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [name, surname, email, hashedPassword, role || "respondent"]
-    );
-    return registeredUser.rows[0];
+    // const registeredUser = await pool.query(
+    //   'INSERT INTO "user" (name, surname, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+    //   [name, surname, email, hashedPassword, role || "respondent"]
+    // );
+    const registeredUser = await prisma.user.create({
+      data: {
+        name: name,
+        surname: surname,
+        email: email,
+        password: hashedPassword,
+        role: role || "responded",
+      },
+    });
+    // return registeredUser.rows[0];
+    return registeredUser;
   } catch (error) {
     console.log("error");
     throw new CustomError("Server error", 500);
